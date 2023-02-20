3db4bdf6c8c4e364be29db3b522efa1fb96ca323
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-02-20 12:45:39 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,5 +1,1 @@
-#set env vars
-# set -o allexport; source .env; set +o allexport;
-
-#wait until the server is ready
 echo "Waiting for software to be ready ..."
--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,6 +1,2 @@
-#set env vars
-#set -o allexport; source .env; set +o allexport;
-
-
 mkdir -p ./data
 chmod a+w data
